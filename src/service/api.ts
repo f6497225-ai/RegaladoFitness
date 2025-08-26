ec82a736a2930e1d2ad5ac72a9090fa93c8f755a
Sevenox <Sevenox@example.com>
2025-08-26 07:38:16 +0200

    ap

--- src/service/api.ts
+++ src/service/api.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://localhost:3000'; // o la URL pública de tu backend
+  private apiUrl = 'https://backend-debe.onrender.com'; // o la URL pública de tu backend
 
   constructor(private http: HttpClient) {}
 
